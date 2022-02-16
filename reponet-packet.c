4fd1c6c042c387fa39cea3c281144fa3dc3b76b0
Farzin <43478550+inPhraZ@users.noreply.github.com>
2022-02-16 12:55:28 +0330

    analyze_packet: allocate

--- reponet-packet.c
+++ reponet-packet.c
@@ -11,20 +11,34 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <pcap/pcap.h>
 #include <arpa/inet.h>
 #include <net/ethernet.h>
 
 #include "reponet-packet.h"
 
+
 Packetptr allocate_packet()
 {
     Packetptr pktptr;
     pktptr = (Packetptr)malloc(sizeof(Packet));
+    if (!pktptr)
+        return NULL;
+    memset(pktptr, 0, sizeof(Packet));
+    return pktptr;
+}
+
+Packetptr   analyze_packet(const struct pcap_pkthdr *h, const u_char *bytes)
+{
+    Packetptr pktptr;
+    pktptr = allocate_packet();
     if (!pktptr) {
-        perror("Allocate Packet");
+        perror("allocate_packet");
         return NULL;
     }
-    memset(pktptr, 0, sizeof(Packet));
+    /*-----------------------------------------------------------------------------
+     * TODO: analyze the packet
+     *-----------------------------------------------------------------------------*/
     return pktptr;
 }
 
