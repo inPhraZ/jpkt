600859f9cc34226eb14fbfc3f4bf40bba967ee73
Farzin <43478550+inPhraZ@users.noreply.github.com>
2022-02-16 11:46:19 +0330

    message->packet

--- reponet-packet.h
+++ reponet-packet.h
@@ -11,6 +11,16 @@
 #define __REPONET_PACKET_H_     1
 
 #include <sys/types.h>
+#include <sys/queue.h>
+
+/* Structure containing captured packet */
+struct reponet_packet {
+    size_t  len;            /*  Length of message */
+    char    *pktmsg;        /* Packet info as JSON */
+    STAILQ_ENTRY(reponet_packet)   entries;
+};
+
+STAILQ_HEAD(packet_queue, reponet_packet);
 
 u_int16_t ethernet_type(u_char *user, const u_char *bytes);
 
