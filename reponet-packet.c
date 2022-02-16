d3692cdd371c983d9a3805ec4a084152c6816ff9
Farzin <43478550+inPhraZ@users.noreply.github.com>
2022-02-16 12:25:08 +0330

    allocate_packet

--- reponet-packet.c
+++ reponet-packet.c
@@ -7,11 +7,28 @@
  * =====================================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <net/ethernet.h>
 
 #include "reponet-packet.h"
 
+Packetptr allocate_packet()
+{
+    Packetptr pktptr;
+    pktptr = (Packetptr)malloc(sizeof(Packet));
+    if (!pktptr) {
+        perror("Allocate Packet");
+        return NULL;
+    }
+    memset(pktptr, 0, sizeof(Packet));
+    return pktptr;
+}
+
+#if 0
 u_int16_t ethernet_type(u_char *user, const u_char *bytes)
 {
     u_int16_t type;
@@ -21,3 +38,5 @@ u_int16_t ethernet_type(u_char *user, const u_char *bytes)
     type = ntohs(ehp->ether_type);
     return type;
 }
+
+#endif
