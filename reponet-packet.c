23f81dee2d49a70959fa0380e1c8717dc2cc8004
Farzin Monsef <monseffarzin@gmail.com>
2022-03-05 18:30:20 +0330

    comments and prototypes

--- reponet-packet.c
+++ reponet-packet.c
@@ -21,11 +21,128 @@
 #include "reponet-eth.h"
 #include "reponet-ip.h"
 
+/*  prototypes for packet routines */
+static uint16_t packet_eth(JsonBuilder *builder, const u_char *bytes);
+static int      packet_ip(JsonBuilder *builder, const u_char *bytes);
+
+/*  dummy function for protocols that have not yet been supported */
 static int dummy_call(JsonBuilder *builder, const u_char *bytes)
 {
     return 0;
 }
 
+/*  function pointers to analyze ethertype protocols
+ *  <net/ethernet.h>
+ */
+static int (*ethertype_protocols[])(JsonBuilder *builder,
+        const u_char *bytes) = {
+    [ETHERTYPE_PUP]         dummy_call,
+    [ETHERTYPE_SPRITE]      dummy_call,
+    [ETHERTYPE_IP]          packet_ip,
+    [ETHERTYPE_ARP]         dummy_call,
+    [ETHERTYPE_REVARP]      dummy_call,
+    [ETHERTYPE_AT]          dummy_call,
+    [ETHERTYPE_AARP]        dummy_call,
+    [ETHERTYPE_VLAN]        dummy_call,
+    [ETHERTYPE_IPX]         dummy_call,
+    [ETHERTYPE_IPV6]        dummy_call,
+    [ETHERTYPE_LOOPBACK]    dummy_call
+};
+
+static packet_t *packet_alloc()
+{
+    packet_t *pktptr;
+    pktptr = (packet_t *)malloc(sizeof(packet_t));
+    if (!pktptr)
+        return NULL;
+    memset(pktptr, 0, sizeof(packet_t));
+    return pktptr;
+}
+
+/*  Analyze raw bytes of traffic and  */
+packet_t   *analyze_packet(const struct pcap_pkthdr *h, const u_char *bytes)
+{
+    packet_t *pktptr;
+    pktptr = packet_alloc();
+    if (!pktptr) {
+        perror("allocate_packet");
+        return NULL;
+    }
+
+    g_autoptr(JsonBuilder) builder = json_builder_new();
+
+    json_builder_begin_object(builder); /*  begin object: main */
+    json_builder_set_member_name(builder, "layers");
+    json_builder_begin_object(builder); /*  begin object: layers */
+
+    /*  ethernet header */
+    uint16_t type = packet_eth(builder, bytes);
+    if (type == 0) {
+        packet_free(pktptr);
+        json_builder_reset(builder);
+        return NULL;
+    }
+
+    /* Skip ethernet header */
+    u_char *tmp_bytes = (u_char *)(bytes + sizeof(struct ether_header));
+
+    ethertype_protocols[type](builder, tmp_bytes);
+
+    /*-----------------------------------------------------------------------------
+     * TODO: analyze the packet
+     *-----------------------------------------------------------------------------*/
+
+    json_builder_end_object(builder);   /*  end of object: layers */
+    json_builder_end_object(builder);   /*  end of object: main */
+
+    g_autoptr(JsonNode) root = json_builder_get_root(builder);
+    g_autoptr(JsonGenerator) gen = json_generator_new();
+
+    json_generator_set_root(gen, root);
+    pktptr->pktmsg = json_generator_to_data(gen, NULL);
+    g_autofree char *tmp = json_generator_to_data(gen, NULL);
+    size_t tlen = strlen(tmp);
+    pktptr->pktmsg = (char *)malloc(tlen+1);
+    memset(pktptr->pktmsg, 0, tlen+1);
+    memmove(pktptr->pktmsg, tmp, tlen);
+    printf("%s\n", pktptr->pktmsg);
+
+    return pktptr;
+}
+
+static uint16_t  packet_eth(JsonBuilder *builder, const u_char *bytes)
+{
+    uint16_t type;
+    ethernet_t *ethp = ethernet_extract(bytes);
+    if (!ethp) {
+        fprintf(stderr, "ethernet_extract failed");
+        return 0;
+    }
+
+    json_builder_set_member_name(builder, "eth");   /*  begin object: eth */
+    json_builder_begin_object(builder);
+
+    /* eth.shost  */
+    json_builder_set_member_name(builder, "eth.shost");
+    json_builder_add_string_value(builder, ethp->shost_str);
+
+    /* eth.dhost  */
+    json_builder_set_member_name(builder, "eth.dhost");
+    json_builder_add_string_value(builder, ethp->dhost_str);
+
+    /* eth.type */
+    json_builder_set_member_name(builder, "eth.type");
+    json_builder_add_string_value(builder, ethp->type_str);
+
+    json_builder_end_object(builder);   /*  end of object: eth */
+
+    type = ethp->type;
+    ethernet_free(ethp);
+
+    return type;
+}
+
+/*  analyze raw bytes to extract ip header */
 static int packet_ip(JsonBuilder *builder, const u_char *bytes)
 {
     ip_t *ipptr;
@@ -93,111 +210,4 @@ static int packet_ip(JsonBuilder *builder, const u_char *bytes)
      * TODO: analyze upper protocol (ip.protocol) 
      *-----------------------------------------------------------------------------*/
     return 0;
-}
-
-static int (*ethertype_protocols[])(JsonBuilder *builder,
-        const u_char *bytes) = {
-    [ETHERTYPE_PUP]         dummy_call,
-    [ETHERTYPE_SPRITE]      dummy_call,
-    [ETHERTYPE_IP]          packet_ip,
-    [ETHERTYPE_ARP]         dummy_call,
-    [ETHERTYPE_REVARP]      dummy_call,
-    [ETHERTYPE_AT]          dummy_call,
-    [ETHERTYPE_AARP]        dummy_call,
-    [ETHERTYPE_VLAN]        dummy_call,
-    [ETHERTYPE_IPX]         dummy_call,
-    [ETHERTYPE_IPV6]        dummy_call,
-    [ETHERTYPE_LOOPBACK]    dummy_call
-};
-
-static packet_t *packet_alloc()
-{
-    packet_t *pktptr;
-    pktptr = (packet_t *)malloc(sizeof(packet_t));
-    if (!pktptr)
-        return NULL;
-    memset(pktptr, 0, sizeof(packet_t));
-    return pktptr;
-}
-
-static uint16_t  packet_eth(JsonBuilder *builder, const u_char *bytes)
-{
-    uint16_t type;
-    ethernet_t *ethp = ethernet_extract(bytes);
-    if (!ethp) {
-        fprintf(stderr, "ethernet_extract failed");
-        return 0;
-    }
-
-    json_builder_set_member_name(builder, "eth");   /*  begin object: eth */
-    json_builder_begin_object(builder);
-
-    /* eth.shost  */
-    json_builder_set_member_name(builder, "eth.shost");
-    json_builder_add_string_value(builder, ethp->shost_str);
-
-    /* eth.dhost  */
-    json_builder_set_member_name(builder, "eth.dhost");
-    json_builder_add_string_value(builder, ethp->dhost_str);
-
-    /* eth.type */
-    json_builder_set_member_name(builder, "eth.type");
-    json_builder_add_string_value(builder, ethp->type_str);
-
-    json_builder_end_object(builder);   /*  end of object: eth */
-
-    type = ethp->type;
-    ethernet_free(ethp);
-
-    return type;
-}
-
-packet_t   *analyze_packet(const struct pcap_pkthdr *h, const u_char *bytes)
-{
-    packet_t *pktptr;
-    pktptr = packet_alloc();
-    if (!pktptr) {
-        perror("allocate_packet");
-        return NULL;
-    }
-
-    g_autoptr(JsonBuilder) builder = json_builder_new();
-
-    json_builder_begin_object(builder); /*  begin object: main */
-    json_builder_set_member_name(builder, "layers");
-    json_builder_begin_object(builder); /*  begin object: layers */
-
-    /*  ethernet header */
-    uint16_t type = packet_eth(builder, bytes);
-    if (type == 0) {
-        packet_free(pktptr);
-        json_builder_reset(builder);
-        return NULL;
-    }
-
-    /* Skip ethernet header */
-    u_char *tmp_bytes = (u_char *)(bytes + sizeof(struct ether_header));
-
-    ethertype_protocols[type](builder, tmp_bytes);
-
-    /*-----------------------------------------------------------------------------
-     * TODO: analyze the packet
-     *-----------------------------------------------------------------------------*/
-
-    json_builder_end_object(builder);   /*  end of object: layers */
-    json_builder_end_object(builder);   /*  end of object: main */
-
-    g_autoptr(JsonNode) root = json_builder_get_root(builder);
-    g_autoptr(JsonGenerator) gen = json_generator_new();
-
-    json_generator_set_root(gen, root);
-    pktptr->pktmsg = json_generator_to_data(gen, NULL);
-    g_autofree char *tmp = json_generator_to_data(gen, NULL);
-    size_t tlen = strlen(tmp);
-    pktptr->pktmsg = (char *)malloc(tlen+1);
-    memset(pktptr->pktmsg, 0, tlen+1);
-    memmove(pktptr->pktmsg, tmp, tlen);
-    printf("%s\n", pktptr->pktmsg);
-
-    return pktptr;
 }
