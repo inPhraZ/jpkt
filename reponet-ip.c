8745050593f63a9d5565a6ad0a85203dbb37b086
Farzin Monsef <monseffarzin@gmail.com>
2022-03-06 17:22:56 +0330

    ip_protocol_ids

--- reponet-ip.c
+++ reponet-ip.c
@@ -16,6 +16,36 @@
 
 #include "reponet-ip.h"
 
+static const char *ip_protocol_ids[] = {
+    [IPPROTO_IP]        "IP",
+    [IPPROTO_ICMP]      "ICMP",
+    [IPPROTO_IGMP]      "IGMP",
+    [IPPROTO_IPIP]      "IPIP",
+    [IPPROTO_TCP]       "TCP",
+    [IPPROTO_EGP]       "EGP",
+    [IPPROTO_PUP]       "PUP",
+    [IPPROTO_UDP]       "UDP",
+    [IPPROTO_IDP]       "IDP",
+    [IPPROTO_TP]        "TP",
+    [IPPROTO_DCCP]      "DCCP",
+    [IPPROTO_IPV6]      "IPv6",
+    [IPPROTO_RSVP]      "RSVP",
+    [IPPROTO_GRE]       "GRE",
+    [IPPROTO_ESP]       "ESP",
+    [IPPROTO_AH]        "AH",
+    [IPPROTO_MTP]       "MTP",
+    [IPPROTO_BEETPH]    "BEETPH",
+    [IPPROTO_ENCAP]     "ENCAP",
+    [IPPROTO_PIM]       "PIM",
+    [IPPROTO_COMP]      "COMP",
+    [IPPROTO_SCTP]      "SCTP",
+    [IPPROTO_UDPLITE]   "UDPLITE",
+    [IPPROTO_MPLS]      "MPLS",
+    [IPPROTO_ETHERNET]  "ETHERNET",
+    [IPPROTO_RAW]       "RAW",
+    [IPPROTO_MPTCP]     "MPTCP"
+};
+
 ip_t *ip_extract(const u_char *bytes)
 {
     ip_t *ipptr;
@@ -57,7 +87,7 @@ ip_t *ip_extract(const u_char *bytes)
     snprintf(ipptr->ttl, IPTTLLEN,
             "%d", ip_header->ip_ttl);
     snprintf(ipptr->protocol, IPPROTOLEN,
-            "%d", ip_header->ip_p);
+            "%s", ip_protocol_ids[ip_header->ip_p]);
     snprintf(ipptr->checksum, IPSUMLEN,
             "0x%x", ntohs(ip_header->ip_sum));
     snprintf(ipptr->saddr, IPADDRLEN,
