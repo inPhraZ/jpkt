5cb605ba30457a442a315fa365baebc8b3573bd1
Farzin Monsef <monseffarzin@gmail.com>
2022-03-07 18:26:38 +0330

    ARPHRD ids

---
+++ reponet-arp.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  reponet-arp.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2022 01:07:15 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/ether.h>
+
+#include "reponet-arp.h"
+
+/*  ARP protocol hardware identifiers. */
+static const char *arp_hardware_ids[] = {
+    [ARPHRD_NETROM]     "NetRom",
+    [ARPHRD_ETHER]      "Ethernet",
+    [ARPHRD_EETHER]     "EEthernet",
+    [ARPHRD_AX25]       "AX25",
+    [ARPHRD_PRONET]     "PROnet",
+    [ARPHRD_CHAOS]      "Chaosnet",
+    [ARPHRD_IEEE802]    "IEEE802",
+    [ARPHRD_ARCNET]     "ARCnet",
+    [ARPHRD_APPLETLK]   "AppleTalk",
+    [ARPHRD_DLCI]       "DLCI",
+    [ARPHRD_ATM]        "ATM",
+    [ARPHRD_METRICOM]   "Metricom",
+    [ARPHRD_IEEE1394]   "IEEE1394",
+    [ARPHRD_EUI64]      "EUI-64",
+    [ARPHRD_INFINIBAND] "Infiniband"
+};
