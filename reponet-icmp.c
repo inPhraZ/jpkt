b4e60a5eedd318da55023073063301631fec5f65
Farzin Monsef <monseffarzin@gmail.com>
2022-03-10 18:24:52 +0330

    icmp type strings

--- reponet-icmp.c
+++ reponet-icmp.c
@@ -9,6 +9,22 @@
 
 #include "reponet-icmp.h"
 
+static const char *icmp_types[] = {
+    [ICMP_ECHOREPLY]        "Echo Reply",
+    [ICMP_DEST_UNREACH]     "Destination Unreachable",
+    [ICMP_SOURCE_QUENCH]    "Source Quench",
+    [ICMP_REDIRECT]         "Redirect",
+    [ICMP_ECHO]             "Echo Request",
+    [ICMP_TIME_EXCEEDED]    "Time Exceeded",
+    [ICMP_PARAMETERPROB]    "Parameter Problem",
+    [ICMP_TIMESTAMP]        "Timestamp Request",
+    [ICMP_TIMESTAMPREPLY]   "Timestamp Reply",
+    [ICMP_INFO_REQUEST]     "Information Request",
+    [ICMP_INFO_REPLY]       "Information Reply",
+    [ICMP_ADDRESS]          "Address Mask Request",
+    [ICMP_ADDRESSREPLY]     "Address Mask Reply"
+};
+
 icmp_t *icmp_extract(const u_char *bytes)
 {
     icmp_t *icmpptr;
