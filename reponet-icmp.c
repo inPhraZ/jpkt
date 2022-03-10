f4b40063957bc1a7bbfe777efa308d277158ec9c
Farzin Monsef <monseffarzin@gmail.com>
2022-03-10 19:36:16 +0330

    codes for types

--- reponet-icmp.c
+++ reponet-icmp.c
@@ -29,6 +29,37 @@ static const char *icmp_types[] = {
     [ICMP_ADDRESSREPLY]     "Address Mask Reply"
 };
 
+static const char *icmp_unreach_codes[] = {
+    [ICMP_NET_UNREACH]      "Network Unreachable",
+    [ICMP_HOST_UNREACH]     "Host Unreachable",
+    [ICMP_PROT_UNREACH]     "Protocol Unreachable",
+    [ICMP_PORT_UNREACH]     "Port Unreachable",
+    [ICMP_FRAG_NEEDED]      "Fragmentation Needed",
+    [ICMP_SR_FAILED]        "Source Route Failed",
+    [ICMP_NET_UNKNOWN]      "Network Unknown",
+    [ICMP_HOST_UNKNOWN]     "Host Unknown",
+    [ICMP_HOST_ISOLATED]    "Host Isolated",
+    [ICMP_NET_ANO]          "Network Prohibited",
+    [ICMP_HOST_ANO]         "Host Prohibited",
+    [ICMP_NET_UNR_TOS]      "Network Unreachable for TOS",
+    [ICMP_HOST_UNR_TOS]     "Host unreachable for TOS",
+    [ICMP_PKT_FILTERED]     "Packet Filtered",
+    [ICMP_PREC_VIOLATION]   "Precedence Violation",
+    [ICMP_PREC_CUTOFF]      "Precedence Cut Off"
+};
+
+static const char *icmp_redirect_codes[] = {
+    [ICMP_REDIR_NET]        "Redirect Net",
+    [ICMP_REDIR_HOST]       "Redirect Host",
+    [ICMP_REDIR_NETTOS]     "Redirect Net for TOS",
+    [ICMP_REDIR_HOSTTOS]    "Redirect Host for TOS"
+};
+
+static const char *icmp_time_exceeded_codes[] = {
+    [ICMP_EXC_TTL]          "TTL count exceeded",
+    [ICMP_EXC_FRAGTIME]     "Fragment time exceeded"
+};
+
 icmp_t *icmp_extract(const u_char *bytes)
 {
     icmp_t *icmpptr;
