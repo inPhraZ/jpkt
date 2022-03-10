f4dfc4d86fed0c9bcf5188669155324936c98ccd
Farzin Monsef <monseffarzin@gmail.com>
2022-03-10 18:17:47 +0330

    icmp: extract, free

--- reponet-icmp.h
+++ reponet-icmp.h
@@ -18,4 +18,16 @@ typedef struct __reponet_icmp {
     char    checksum[ICMPSUMLEN];
 } icmp_t;
 
+/* Allocate memory for icmp header and extract fields from bytes */
+icmp_t  *icmp_extract(const u_char *bytes);
+
+/*  Free allocated memory and assign NULL to icmpptr */
+#define icmp_free(icmpptr)  \
+    do {                    \
+        free(icmpptr->type);\
+        free(icmpptr->code);\
+        free(icmpptr);      \
+        icmpptr = NULL;     \
+    } while(0)
+
 #endif      /*  __REPONET_ICMP_H_ */
