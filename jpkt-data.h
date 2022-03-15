5f310ed96e24f72c52032fb1917bbdab3692dc6d
Farzin <monseffarzin@gmail.com>
2022-03-15 17:06:01 +0330

    data

---
+++ jpkt-data.h
@@ -0,0 +1,16 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jpkt-data.h
+ *         Author:  Farzin 
+ *
+ * =====================================================================================
+ */
+
+#ifndef 	__JPACKET_DATA_H_
+#define 	__JPACKET_DATA_H_	1
+
+char *data_to_string(const u_char *bytes,
+		const uint16_t slen, uint16_t *dlen);
+
+#endif		/*  __JPACKET_DATA_H_  */
