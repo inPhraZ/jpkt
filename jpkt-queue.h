92068c56d508830d8a66b026ca388051dabbe97f
Farzin <monseffarzin@gmail.com>
2022-03-23 20:26:15 +0430

    prototypes

--- jpkt-queue.h
+++ jpkt-queue.h
@@ -18,5 +18,20 @@
 STAILQ_HEAD(__packet_queue, __jpkt_packet);
 typedef struct __packet_queue jpkt_queue;
 
+/* initialize new queue */
+jpkt_queue	*packet_init_queue();
+
+/* insert pkt at tail of packets queue */
+void 		 packet_enqueue(jpkt_queue *packets, const packet_t *pkt);
+
+/* remove head from packets queue  */
+packet_t	*packet_dequeue(jpkt_queue *packets);
+
+/* free packets queue */
+void 		__packet_free_queue(jpkt_queue *packets);
+
+/* free packets queue and assign NULL to pointer */
+#define 	packet_free_queue(packets)	\
+	do { __packet_free_queue(packets); packets = NULL; } while(0)
 
 #endif 		/*  __JPACKET_QUEUE_H_ */
