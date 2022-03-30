9542e6da474ade9ce3d9d15339f9b61350669ce1
Farzin <monseffarzin@gmail.com>
2022-03-31 00:21:12 +0430

    jpkt_sniff: prototype

--- jpkt.h
+++ jpkt.h
@@ -10,4 +10,11 @@
 #ifndef 		__JPACKET_H_
 #define 		__JPACKET_H_	1
 
+typedef void (*jpkt_handler)(void *, const char *, const size_t);
+
+int jpkt_sniff(const char *iface,
+		unsigned int count,
+		jpkt_handler,
+		void *user);
+
 #endif		/*  __JPACKET_H_ */
