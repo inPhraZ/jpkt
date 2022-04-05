d810940123447e973a142373ed790a40d3e650b6
Farzin <monseffarzin@gmail.com>
2022-04-05 19:32:54 +0430

    jpkt_findalldevs

--- jpkt.h
+++ jpkt.h
@@ -10,7 +10,11 @@
 #ifndef 		__JPACKET_H_
 #define 		__JPACKET_H_	1
 
+typedef pcap_if_t jpkt_if_t;
+
 typedef void (*jpkt_handler)(void *, const char *, const size_t);
+
+int jpkt_findalldevs(jpkt_if_t **alldevsp, char *errbuf);
 
 int jpkt_sniff(const char *iface,
 		unsigned int count,
