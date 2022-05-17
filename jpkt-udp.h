ed535450a68c7471136271c426a04d1d8cb8c458
Farzin <monseffarzin@gmail.com>
2022-05-18 00:02:24 +0430

    udp_t, prototype

--- jpkt-udp.h
+++ jpkt-udp.h
@@ -10,4 +10,17 @@
 #ifndef 	__JPACKET_UDP_H_
 #define 	__JPACKET_UDP_H_
 
+#include <netinet/udp.h>
+
+#define 	UDPSUMLEN	7
+
+typedef struct __jpkt_udp {
+	uint16_t uh_sport;
+	uint16_t uh_dport;
+	uint16_t uh_ulen;
+	char 	 uh_sum[UDPSUMLEN];
+} udp_t;
+
+udp_t *udp_extract(const u_char *bytes);
+
 #endif		/*  __JPACKET_UDP_H_  */
