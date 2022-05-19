e012aae14ffd8e552e49772cea7b53ebc76b73c2
Farzin <monseffarzin@gmail.com>
2022-05-19 17:27:24 +0430

    header guard

---
+++ jpkt-tcp.h
@@ -0,0 +1,17 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jpkt-tcp.h
+ *         Author:  Farzin 
+ *
+ * =====================================================================================
+ */
+
+#ifndef 	__JPACKET_TCP_H_
+#define 	__JPACKET_TCP_H_	1
+
+typedef struct __jpkt_tcp {
+	/*	tcp fields */
+} tcp_t;
+
+#endif		/*  __JPACKET_TCP_H_ */
