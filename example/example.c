ff84cd6385c0c5eef6622afa01f5f54cc54fbb49
Farzin <monseffarzin@gmail.com>
2022-07-02 01:02:27 +0430

    fix indent

--- example/example.c
+++ example/example.c
@@ -21,19 +21,19 @@ void callback(void *user, const char *pkt, const size_t len)
 
 int main()
 {
-    char ifname[IF_NAMESIZE + 1];
-    char errbuf[PCAP_ERRBUF_SIZE];
+	char ifname[IF_NAMESIZE + 1];
+	char errbuf[PCAP_ERRBUF_SIZE];
 
-    jpkt_if_t *alldevsp = NULL;
-    if (jpkt_findalldevs(&alldevsp, errbuf)) {
-        fprintf(stderr, "pcap_init: %s\n", errbuf);
-        exit(EXIT_FAILURE);
-    }
+	jpkt_if_t *alldevsp = NULL;
+	if (jpkt_findalldevs(&alldevsp, errbuf)) {
+		fprintf(stderr, "pcap_init: %s\n", errbuf);
+		exit(EXIT_FAILURE);
+	}
 
-    if (alldevsp == NULL) {
-        fprintf(stderr, "No devices were found\n");
-        exit(EXIT_SUCCESS);
-    }
+	if (alldevsp == NULL) {
+		fprintf(stderr, "No devices were found\n");
+		exit(EXIT_SUCCESS);
+	}
 
 	jpkt_if_t *tmp = alldevsp;
 	while(tmp) {
@@ -41,11 +41,11 @@ int main()
 		tmp = tmp->next;
 	}
 	memset(ifname, 0, IF_NAMESIZE + 1);
-    strncpy(ifname, alldevsp->name, IF_NAMESIZE);
-    jpkt_freealldevs(alldevsp);
+	strncpy(ifname, alldevsp->name, IF_NAMESIZE);
+	jpkt_freealldevs(alldevsp);
 
 	int a = 10;
 	jpkt_sniff(ifname, 0, callback, &a);
 
-    return 0;
+	return 0;
 }
